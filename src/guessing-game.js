88f7e861f45001e841629e3f5e4ba5f62d8b0410
MarinaKorniychuk <kornij4uk@gmail.com>
2017-01-31 21:10:05 +0300

    Complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this._low = null;
+    	this._high = null;
+    	this._mid = null;
+    }
 
     setRange(min, max) {
-
+    	this._low = min;
+    	this._high = max;
     }
 
     guess() {
-
+    	this._mid = Math.ceil((this._low + this._high) / 2);
+    	return this._mid;
     }
 
     lower() {
-
+    	this._high = this._mid;
+        return this._high;
     }
 
     greater() {
-
+    	this._low = this._mid;
+        return this._low;
     }
 }
 
